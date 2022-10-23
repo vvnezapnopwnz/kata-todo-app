bb6177a981b82f4c5b3066275a4d729f0daa2fc2
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2022-10-23 18:22:42 +0600

    task_timer

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,12 +1,48 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { formatDistanceToNowStrict } from 'date-fns'
+import { formatDistanceToNowStrict, format, addSeconds } from 'date-fns'
 import './Task.css'
 
 export default class Task extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timerCount: 0,
+      timerStatus: false,
+    }
+    this.onPlayClick = () => {
+      const { timerStatus } = this.state
+      if (timerStatus === false) {
+        this.setState({ timerStatus: true })
+        this.interval = setInterval(() => {
+          const { timerCount } = this.state
+          const newTimerValue = timerCount + 1
+          this.setState({ timerCount: newTimerValue })
+        }, 1000)
+      }
+    }
+
+    this.onPauseClick = () => {
+      const { timerStatus } = this.state
+      if (timerStatus === true) {
+        this.setState({ timerStatus: false })
+        clearInterval(this.interval)
+      }
+    }
+
+    this.formattedTime = (seconds) => {
+      const helperDate = addSeconds(new Date(0), seconds)
+      return format(helperDate, 'mm:ss')
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
   render() {
     const { id, taskStatus, label, onDelete, onComplete, onEdit, editInput, editSubmit, createdAt } = this.props
-
+    const { timerCount } = this.state
     const onSubmitHandler = (event) => {
       event.preventDefault()
       editSubmit(id, label)
@@ -43,6 +79,11 @@ export default class Task extends PureComponent {
           />
           <label htmlFor={`task-${id}`}>
             <span className="description">{label}</span>
+            <span className="timer">
+              <button className="icon icon-play" type="button" onClick={() => this.onPlayClick()} />
+              <button className="icon icon-pause" type="button" onClick={this.onPauseClick} />
+              <span>{this.formattedTime(timerCount)}</span>
+            </span>
             <span className="created">
               created {formatDistanceToNowStrict(createdAt, { includeSeconds: true, addSuffix: false })} ago
             </span>
